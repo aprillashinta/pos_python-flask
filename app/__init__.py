3dc96a89027bed486281ddb9aedafab9aefd0674
aprillashinta <aprillaaddivi@gmail.comm>
2025-11-30 11:24:20 +0700

    commit session 8-11

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from config import config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config.from_object(config)
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app.model import user
 from app import routes
